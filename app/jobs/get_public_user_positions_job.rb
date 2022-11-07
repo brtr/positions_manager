f74c0624f1d7ff16be384812df271236c4cd3e4b
Hazel <xp0729@gmail.com>
2022-11-07 20:41:39 +1100

    update get public positions job

--- app/jobs/get_public_user_positions_job.rb
+++ app/jobs/get_public_user_positions_job.rb
@@ -12,7 +12,7 @@ class GetPublicUserPositionsJob < ApplicationJob
       from_symbol = d["symbol"].split(fee_symbol)[0]
       qty = d["positionAmt"].to_f
       t_type = qty > 0 ? "sell" : "buy"
-      up = UserPosition.where(origin_symbol: d["symbol"], fee_symbol: fee_symbol, from_symbol: from_symbol, trade_type: t_type, source: "binance").first_or_create
+      up = get_up(d["symbol"], fee_symbol, from_symbol, t_type, 'binance')
       ids.push(up.id)
       up.update(price: d["entryPrice"].to_f, qty: qty.abs)
 
@@ -23,7 +23,7 @@ class GetPublicUserPositionsJob < ApplicationJob
       from_symbol, fee_symbol = d["instId"].split('-')
       qty = (d["notionalUsd"].to_f / d["last"].to_f).round(0)
       t_type = d["posSide"] == "long" || (d["posSide"] == "net" && d["pos"].to_f > 0) ? "sell" : "buy"
-      up = UserPosition.where(origin_symbol: d["instId"], fee_symbol: fee_symbol, from_symbol: from_symbol, trade_type: t_type, source: "okx").first_or_create
+      up = get_up(d["instId"], fee_symbol, from_symbol, t_type, 'okx')
       ids.push(up.id)
       up.update(price: d["avgPx"].to_f, qty: qty.abs)
 
@@ -37,7 +37,7 @@ class GetPublicUserPositionsJob < ApplicationJob
       size = get_contract_size(symbol).to_f rescue 1
       qty = d["volume"] * size
       t_type = d["direction"] == "buy" ? "sell" : "buy"
-      up = UserPosition.where(origin_symbol: symbol, fee_symbol: fee_symbol, from_symbol: from_symbol, trade_type: t_type, source: "huobi").first_or_create
+      up = get_up(symbol, fee_symbol, from_symbol, t_type, 'huobi')
       ids.push(up.id)
       up.update(price: price, qty: qty.abs)
 
@@ -67,5 +67,9 @@ class GetPublicUserPositionsJob < ApplicationJob
 
   def get_info(up)
     UserPositionService.get_info(up)
+  end
+
+  def get_up(symbol, fee_symbol, from_symbol, trade_type, source)
+    UserPosition.where(user_id: nil, origin_symbol: symbol, fee_symbol: fee_symbol, from_symbol: from_symbol, trade_type: trade_type, source: source).first_or_create
   end
 end
