df1944a5d69e3f7ddf84246dfbba6b69fc3c852a
Hazel <xp0729@gmail.com>
2022-12-26 21:39:21 +1100

    add test

---
+++ spec/controllers/combine_transactions_controller_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe CombineTransactionsController, type: :controller do
+  let(:user) { create(:user) }
+  before do
+    sign_in user
+    10.times{ create(:combine_transaction) }
+  end
+
+  describe "GET index" do
+    it "renders a successful response" do
+      get :index
+
+      expect(assigns(:txs).count).to eq(10)
+      expect(response).to be_successful
+      expect(response).to render_template(:index)
+    end
+  end
+end
