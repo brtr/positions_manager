1fd87be7b893823eb099bffb649b589fa4d0bd52
Hazel <xp0729@gmail.com>
2023-02-02 20:49:26 +1100

    update recently adding positions page https://github.com/brtr/coin_elite/issues/615

--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -1,5 +1,6 @@
 module PageHelper
   def adding_positions_summary(data)
+    return {} if data.empty?
     max_amount_record = data.sort_by{|d| d["amount"].to_f}.reverse.first
     total_amount = data.sum{|d| d["amount"].to_f}
     rate = data.sum{|d| d["revenue"].to_f} / total_amount
@@ -7,7 +8,7 @@ module PageHelper
       total_amount: total_amount.round(3),
       max_symbol: max_amount_record["symbol"],
       max_amount: max_amount_record["amount"].to_f.round(3),
-      rate: (rate.abs * 100).round(3)
+      rate: (rate * 100).round(3)
     }
   end
 end
--- app/jobs/get_recently_adding_positions_job.rb
+++ app/jobs/get_recently_adding_positions_job.rb
@@ -14,11 +14,16 @@ class GetRecentlyAddingPositionsJob < ApplicationJob
       open_amount = snapshot&.price.to_f * margin_qty
       last_amount = margin_qty * h.current_price
       revenue = snapshot.nil? ? h.revenue : h.trade_type == 'sell' ? last_amount - open_amount : open_amount - last_amount
+      price = margin_amount / margin_qty
       result.push({
         symbol: h.origin_symbol,
+        source: h.source,
+        price: price.round(3),
         qty: margin_qty.round(3),
         revenue: revenue.round(3),
-        amount: margin_amount
+        amount: margin_amount,
+        roi: ((revenue / margin_amount) * 100).round(3),
+        amount_ratio: ((margin_amount / h.amount) * 100).round(3)
       })
     end
 
