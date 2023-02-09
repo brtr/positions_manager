1e6c8b2dd34cfe9b8f76ac82583b2b312ea3066b
Hazel <xp0729@gmail.com>
2023-02-09 20:01:21 +1100

    remove unused servers

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,3 @@
-server "3.237.88.17", user: "deploy", roles: %w(web)
 server "35.72.179.245", user: "deploy", roles: %w(web)
-server "18.215.254.66", user: "deploy", roles: %w(job)
 server "35.72.33.179", user: "deploy", roles: %w(job)
 
