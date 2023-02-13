b98a795b277d7aa5879c49c1ba11dd2807ee4da6
Hazel <xp0729@gmail.com>
2023-02-13 20:11:03 +1100

    add revenue data chart https://github.com/brtr/coin_elite/issues/621

--- app/services/get_price_chart_data_service.rb
+++ app/services/get_price_chart_data_service.rb
@@ -2,7 +2,7 @@ class GetPriceChartDataService
   class << self
     def execute(durations = 1)
       to_date = Date.yesterday
-      from_date = durations.to_i == 3 ? to_date - 3.month : to_date - 1.months
+      from_date = durations.to_i == 3 ? to_date - 3.months : to_date - 1.month
       url = ENV['COIN_ELITE_URL'] + "/api/coins/history_price?symbol=btc&from_date=#{from_date}&to_date=#{to_date}"
       response = RestClient.get(url)
       btc_data = JSON.parse(response.body)
@@ -13,9 +13,13 @@ class GetPriceChartDataService
         position_data[target_date.to_s] = GetAddingPositionsService.execute(date, target_date, true)
       end
 
+      revenue_data = {}
+      SnapshotInfo.where(source_type: 'synced', user_id: nil, event_date: (from_date..to_date)).order(event_date: :asc)
+                  .map{|info| revenue_data[info.event_date.to_s] = info.snapshot_positions.total_summary[:total_revenue]}
+
       result = {}
       btc_data["result"].each do |date, price|
-        result[date] = { btc_price: price, position_amount: position_data[date].to_f }
+        result[date] = { btc_price: price, position_amount: position_data[date].to_f, revenue_data: revenue_data[date].to_f }
       end
 
       $redis.set("monthly_chart_data", result.to_json)
