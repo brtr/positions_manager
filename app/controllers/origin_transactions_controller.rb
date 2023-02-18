04cf93a85dd67236c887091c0a77ddb6b67b68ee
Hazel <xp0729@gmail.com>
2023-02-18 20:43:39 +1100

    update spot transactions current price https://github.com/brtr/coin_elite/issues/625

--- app/controllers/origin_transactions_controller.rb
+++ app/controllers/origin_transactions_controller.rb
@@ -4,8 +4,9 @@ class OriginTransactionsController < ApplicationController
     sort = params[:sort].presence || "event_time"
     sort_type = params[:sort_type].presence || "desc"
     txs = OriginTransaction.where(trade_type: 'buy').order("#{sort} #{sort_type}")
-    txs = txs.where(campaign: params[:campaign]) if params[:campaign]
-    txs = txs.where(source: params[:source]) if params[:source]
+    txs = txs.where(campaign: params[:campaign]) if params[:campaign].present?
+    txs = txs.where(source: params[:source]) if params[:source].present?
+    txs = txs.where(from_symbol: params[:search].upcase) if params[:search].present?
     @txs = txs.page(params[:page]).per(20)
     @total_summary = txs.total_summary
   end
--- app/jobs/get_spot_trades_job.rb
+++ app/jobs/get_spot_trades_job.rb
@@ -7,22 +7,28 @@ class GetSpotTradesJob < ApplicationJob
   def perform(symbol)
     return if symbol.in?(SKIP_SYMBOLS)
     origin_symbol = "#{symbol}#{TRADE_SYMBOL}"
+    txs = OriginTransaction.where(source: SOURCE, original_symbol: origin_symbol)
     trades = BinanceSpotsService.get_my_trades(origin_symbol, from_date: Date.yesterday)
     if trades.is_a?(String) || trades.blank?
       Rails.logger.debug "获取不到#{origin_symbol}的交易记录"
+      txs.each{|tx| update_tx(tx)} if txs.any?
+      combine_trades(origin_symbol)
       return false
     end
 
+    ids = []
     OriginTransaction.transaction do
       trades.each do |trade|
         trade_type = trade[:isBuyer] ? 'buy' : 'sell'
         tx = OriginTransaction.where(source: SOURCE, order_id: trade[:orderId])
                               .first_or_create(original_symbol: trade[:symbol], from_symbol: symbol, to_symbol: TRADE_SYMBOL, fee_symbol: trade[:commissionAsset], trade_type: trade_type,
                                                price: trade[:price], qty: trade[:qty], amount: trade[:quoteQty], fee: trade[:commission], event_time: Time.at(trade[:time] / 1000))
-        tx.current_price = get_current_price(trade[:symbol])
-        tx.revenue = tx.current_price * tx.qty - tx.amount
-        tx.roi = tx.revenue / tx.amount
-        tx.save
+        update_tx(tx)
+        ids.push(tx.id)
+      end
+
+      txs.where.not(id: ids).each do |tx|
+        update_tx(tx)
       end
 
       combine_trades(origin_symbol)
@@ -41,6 +47,13 @@ class GetSpotTradesJob < ApplicationJob
     price
   end
 
+  def update_tx(tx)
+    tx.current_price = get_current_price(tx.original_symbol)
+    tx.revenue = tx.current_price * tx.qty - tx.amount
+    tx.roi = tx.revenue / tx.amount
+    tx.save
+  end
+
   def combine_trades(symbol)
     CombineTransaction.transaction do
       total_cost = 0
@@ -48,6 +61,7 @@ class GetSpotTradesJob < ApplicationJob
       total_fee = 0
 
       origin_txs = OriginTransaction.where(source: SOURCE, original_symbol: symbol).order(event_time: :asc)
+      return if origin_txs.empty?
       origin_txs.each do |tx|
         if tx.trade_type == 'buy'
           total_cost += tx.amount
