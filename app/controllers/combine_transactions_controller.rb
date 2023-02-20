2af194e16ad07bf8bfc403b60c14fef3f53f7ea2
Hazel <xp0729@gmail.com>
2023-02-20 19:33:20 +1100

    add from symbol search to combine transactions list https://github.com/brtr/coin_elite/issues/625

--- app/controllers/combine_transactions_controller.rb
+++ app/controllers/combine_transactions_controller.rb
@@ -4,7 +4,8 @@ class CombineTransactionsController < ApplicationController
     sort = params[:sort].presence || "revenue"
     sort_type = params[:sort_type].presence || "desc"
     txs = CombineTransaction.order("#{sort} #{sort_type}")
-    txs = txs.where(source: params[:source]) if params[:source]
+    txs = txs.where(source: params[:source]) if params[:source].present?
+    txs = txs.where(from_symbol: params[:search].upcase) if params[:search].present?
     @txs = txs.page(params[:page]).per(20)
     @total_summary = txs.total_summary
   end
