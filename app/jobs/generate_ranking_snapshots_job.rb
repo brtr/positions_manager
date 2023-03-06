8b26b02adcadbcc52bb7b1107feb0aede3162c3b
Hazel <xp0729@gmail.com>
2023-03-06 20:09:04 +1100

    fix ranking snapshot is_top10 logic

--- app/jobs/generate_ranking_snapshots_job.rb
+++ app/jobs/generate_ranking_snapshots_job.rb
@@ -6,7 +6,8 @@ class GenerateRankingSnapshotsJob < ApplicationJob
       data = JSON.parse($redis.get("get_24hr_tickers")) rescue []
       data.sort_by{|d| d["priceChangePercent"].to_f}.reverse.each_with_index do |d, idx|
         snapshot = RankingSnapshot.where(event_date: date, symbol: d["symbol"], source: d["source"]).first_or_create
-        snapshot.update(open_price: d["openPrice"], last_price: d["lastPrice"], is_top10: idx < 10,
+        is_top10 = idx < 10 && d["priceChangePercent"].to_f > 10
+        snapshot.update(open_price: d["openPrice"], last_price: d["lastPrice"], is_top10: is_top10,
                         price_change_rate: d["priceChangePercent"], bottom_price_ratio: d["bottomPriceRatio"])
       end
     end
