255f42b8325c209746c602a74fe3115303804786
Hazel <xp0729@gmail.com>
2023-06-29 19:52:10 +1000

    Update funding fee chart https://github.com/brtr/coin_elite/issues/685

--- app/models/funding_fee_history.rb
+++ app/models/funding_fee_history.rb
@@ -4,7 +4,7 @@ class FundingFeeHistory < ApplicationRecord
   delegate :revenue, to: :snapshot_position
 
   def self.data_summary
-    amount_list = FundingFeeHistory.pluck(:amount).sort
+    amount_list = FundingFeeHistory.all.group_by(&:event_date).map{|date, value| value.sum(&:amount)}.sort
     midpoint = amount_list.size / 2.0
     median = amount_list.size.even? ? amount_list[midpoint, 2].sum / 2.0 : amount_list[midpoint]
     {
