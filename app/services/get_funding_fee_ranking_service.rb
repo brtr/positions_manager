587fa0a3d7fa8136b6e07ea7ee175c36633be8de
Hazel <xp0729@gmail.com>
2023-07-06 19:36:29 +1000

    Fix funding fee ranking https://github.com/brtr/coin_elite/issues/688

--- app/services/get_funding_fee_ranking_service.rb
+++ app/services/get_funding_fee_ranking_service.rb
@@ -4,7 +4,9 @@ class GetFundingFeeRankingService
       redis_key = 'binance_future_funding_fee_list'
       rate_list = $redis.get(redis_key)
       if rate_list.nil?
-        rate_list = BinanceFuturesService.new.get_funding_rate(nil, Date.today.strftime('%Q')).to_json
+        rate_list = BinanceFuturesService.new.get_funding_rate(nil, Date.today.strftime('%Q'))
+        last_funding_time = rate_list.last['fundingTime']
+        rate_list = rate_list.select{ |r| r['fundingTime'] == last_funding_time }.to_json
 
         $redis.set(redis_key, rate_list, ex: 2.hours)
       end
