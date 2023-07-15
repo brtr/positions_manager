02a7bc44a1d330abbdf2caef68fabe497fd5cd09
Hazel <xp0729@gmail.com>
2023-07-15 20:29:54 +1000

    fix test

--- features/step_definitions/origin_transactions.rb
+++ features/step_definitions/origin_transactions.rb
@@ -1,4 +1,4 @@
 Given 'I have 10 origin transactions' do
   create_list(:origin_transaction, 9, to_symbol: "USDT", campaign: 'test1')
-  create(:origin_transaction, from_symbol: "BTC", to_symbol: "USDT", revenue: 10, roi: 1, campaign: 'test2')
+  create(:origin_transaction, from_symbol: "BTC", to_symbol: "USDT", revenue: 10, roi: 1, amount: 120, campaign: 'test2')
 end
