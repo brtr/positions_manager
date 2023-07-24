f02d9b9a9da68e8b36a12bff486a4387a1cf3d9b
Hazel <xp0729@gmail.com>
2023-07-24 23:02:19 +1000

    Update get recently adding positions job

--- app/jobs/get_recently_adding_positions_job.rb
+++ app/jobs/get_recently_adding_positions_job.rb
@@ -3,21 +3,21 @@ class GetRecentlyAddingPositionsJob < ApplicationJob
 
   def perform(date: Date.today)
     SyncedTransaction.where(user_id: nil, event_time: (date - 1.day).all_day).group_by{|tx| [tx.origin_symbol, tx.fee_symbol, tx.position_side, tx.source]}.each do |key, txs|
-      from_symbol = key[0].split(key[1])[0]
+      origin_symbol = key[0]
+      from_symbol = origin_symbol.split(key[1])[0]
       trade_type = key[2] == 'short' ? 'buy' : 'sell'
       qty = txs.sum(&:qty)
       amount = txs.sum(&:amount)
       revenue = txs.sum(&:revenue)
       price = amount / qty
-      unless AddingPositionsHistory.exists?(event_date: date - 1.day, origin_symbol: key[0], qty: qty, amount: amount)
-        aph = AddingPositionsHistory.where(event_date: date, origin_symbol: key[0], from_symbol: from_symbol,
-                                          fee_symbol: key[1], trade_type: trade_type, source: key[3]).first_or_create
-        up = UserPosition.where(user_id: nil, origin_symbol: key[0], trade_type: trade_type, source: key[3]).take
-        unit_cost = up&.price || get_last_price(key[0], date)
-        current_price = up&.current_price || get_history_price(from_symbol.downcase, date)
-        aph.update(price: price, current_price: current_price, qty: qty, amount: amount, revenue: revenue, unit_cost: unit_cost)
-        SlackService.send_notification(nil, enqueued_block(key[0])) if AddingPositionsHistory.where("id != ? and event_date = ? and origin_symbol = ? and amount BETWEEN ? AND ?", aph.id, date, key[0], amount * 0.95, amount * 1.05).any?
-      end
+      next if AddingPositionsHistory.where(event_date: date - 1.day, origin_symbol: origin_symbol, qty: qty, amount: amount).any?
+
+      aph = AddingPositionsHistory.where(event_date: date, origin_symbol: origin_symbol, from_symbol: from_symbol, fee_symbol: key[1], trade_type: trade_type, source: key[3]).first_or_create
+      up = UserPosition.where(user_id: nil, origin_symbol: origin_symbol, trade_type: trade_type, source: key[3]).take
+      unit_cost = up&.price || get_last_price(origin_symbol, date)
+      current_price = up&.current_price || get_history_price(from_symbol.downcase, date)
+      aph.update(price: price, current_price: current_price, qty: qty, amount: amount, revenue: revenue, unit_cost: unit_cost)
+      SlackService.send_notification(nil, enqueued_block(from_symbol)) if AddingPositionsHistory.where("id != ? and event_date = ? and origin_symbol = ? and amount BETWEEN ? AND ?", aph.id, date, from_symbol, amount * 0.95, amount * 1.05).any?
     end
   end
 
