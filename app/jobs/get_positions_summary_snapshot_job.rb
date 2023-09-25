d646c44ea3250fa06e25882447d928af5873b819
Pan Xiong <xp0729@gmail.com>
2023-09-25 19:46:40 +1000

    update snapshot summary job default date

--- app/jobs/get_positions_summary_snapshot_job.rb
+++ app/jobs/get_positions_summary_snapshot_job.rb
@@ -3,7 +3,7 @@ require 'rest-client'
 class GetPositionsSummarySnapshotJob < ApplicationJob
   queue_as :daily_job
 
-  def perform(date = Date.today)
+  def perform(date = Date.yesterday)
     GetPositionsSummarySnapshotsService.execute(date)
     ForceGcJob.perform_later
   end
