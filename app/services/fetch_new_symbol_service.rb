1dd85a1b64cb679e0eb933e94e576ed4f0c5be6e
Pan Xiong <xp0729@gmail.com>
2023-11-06 21:16:04 +1100

    Update binance new symbol service

--- app/services/fetch_new_symbol_service.rb
+++ app/services/fetch_new_symbol_service.rb
@@ -9,7 +9,7 @@ class FetchNewSymbolService
       redis_key = 'binance_announcement_fetch_time'
       last_fetch_time = $redis.get(redis_key).to_i
       page = Nokogiri::HTML(URI.open(BASE_URL))
-      data = JSON.parse(page.css("script").children[10])
+      data = JSON.parse(page.css("script").children[13])
       catalog = data['appState']['loader']['dataByRouteId']['2a3f']['catalogs'].select{|c| c['catalogName'] == '数字货币及交易对上新'}.first rescue nil
       return if catalog.nil?
 
