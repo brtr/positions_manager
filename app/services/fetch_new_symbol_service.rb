ddf1a2a16e743193b706fd791f40ae2e6f8331ba
Pan Xiong <xp0729@gmail.com>
2024-03-04 17:55:36 +0800

    Update fetch new symbol service

--- app/services/fetch_new_symbol_service.rb
+++ app/services/fetch_new_symbol_service.rb
@@ -9,8 +9,8 @@ class FetchNewSymbolService
       redis_key = 'binance_announcement_fetch_time'
       last_fetch_time = $redis.get(redis_key).to_i
       page = Nokogiri::HTML(URI.open(BASE_URL), nil, 'UTF-8')
-      data = JSON.parse(page.css("script").children[13])
-      catalog = data['appState']['loader']['dataByRouteId']['2a3f']['catalogs'].select{|c| c['catalogName'] == '数字货币及交易对上新'}.first rescue nil
+      data = JSON.parse(page.css("script").children[15])
+      catalog = data['appState']['loader']['dataByRouteId']['d969']['catalogs'].select{|c| c['catalogName'] == '数字货币及交易对上新'}.first rescue nil
       return if catalog.nil?
 
       $redis.set(redis_key, Time.current.to_i * 1000)
